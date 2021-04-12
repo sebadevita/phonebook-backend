3bb223879897226bb1fd81e3c8e539bff601b216
Sebastian De Vita <sebassdevita@gmail.com>
2021-04-12 14:33:31 -0300

    Terminado ejercicio 3.2 phonebook

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-const http = require('http')
-const express = require('express')
+const http = require("http")
+const express = require("express")
 const app = express()
 
-
- let persons = [
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "12-34567-8"
-      },
-      {
-        "id": 99,
-        "name": "Adam Smith",
-        "number": "12-34567-8"
-      }
-    ]
-
+let persons = [
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "12-34567-8",
+  },
+  {
+    id: 99,
+    name: "Adam Smith",
+    number: "12-34567-8",
+  },
+]
 
 // const app = http.createServer((request, response) => {
 //   response.writeHead(200, { 'Content-Type': 'application/json' })
 //   response.end(JSON.stringify(persons))
 // })
 
-app.get('/', (request, response) => {
-    response.send('<h1> Welcome to phonebook API</h1>')
+app.get("/", (request, response) => {
+  response.send("<h1> Welcome to phonebook API</h1>")
 })
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get("/api/persons", (request, response) => {
+  response.json(persons)
 })
 
-
+app.get("/info", (request, response) => {
+  response.send("<p> Phonebook has info " + persons.length + " for people </p>" + new Date())
+})
 
 const PORT = 3001
 app.listen(PORT)
