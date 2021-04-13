35cff0009ccbab360971094e07dd486b827a0ef8
Sebastian De Vita <sebassdevita@gmail.com>
2021-04-13 09:49:54 -0300

    Terminado ejercicio 3.7

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const http = require("http")
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
+
+morgan.token('host', function(request,response){
+   console.log(request.hostname)
+})
 
 let persons = [
   {
