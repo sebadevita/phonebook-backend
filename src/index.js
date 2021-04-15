4d0c574536de5e186c7d218ad892b391d43af177
Sebastian De Vita <sebassdevita@gmail.com>
2021-04-15 09:06:34 -0300

    Terminado ejercicio 3.12

--- src/index.js
+++ src/index.js
@@ -103,6 +103,6 @@ const personAlreadyExists = (name) =>{
 }
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
